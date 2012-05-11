2732575fe6de90ccc7ec22f8fffacaef44f6e32a
rvlander <rvlander@myhost.localdomain>
2012-05-11 11:15:02 +0200

    partial support for sort

--- MCJavaCore/src/org/mc/mcfunctions/MCJFunctions.java
+++ MCJavaCore/src/org/mc/mcfunctions/MCJFunctions.java
@@ -7,6 +7,7 @@ package org.mc.mcfunctions;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
+import java.util.Arrays;
 import org.mc.mcjavacore.MCJBaseFunctions;
 import org.mc.mcjavacore.MCJElemPosition;
 import org.mc.mcjavacore.MCJMatrixDimensionsException;
@@ -293,7 +294,7 @@ public class MCJFunctions {
         return res;
     }
 
-    public static double[][] sum(MCJOutputArg[] oargs, double[][][] iargs) {
+    public static double[][] sum(MCJOutputArg[] oargs, double[][][] iargs) throws Exception {
         double[][] nargin = MCJBaseFunctions.matrixFromDouble(iargs.length);
         double[][] A = iargs[0];
         if (nargin[0][0] > 0) {
@@ -318,6 +319,45 @@ public class MCJFunctions {
                     res[0][i] += A[j][i];
                 }
             }
+        }
+
+      //  MCJUtils.printMatrix(iargs[1]);
+        
+        /*double[][] dim = new double[0][0];
+        if (iargs.length == 2) {
+            res = MCJBaseFunctions.matrixFromDouble(res[0][(int) iargs[1][0][0] - 1]);
+        }*/
+
+        if (oargs != null) {
+            if (oargs.length == 1) {
+                oargs[0].val = res;
+            }
+        }
+
+        return res;
+    }
+    
+    
+    public static double[][] sort(MCJOutputArg[] oargs, double[][][] iargs) throws Exception{
+        double[][] nargin = MCJBaseFunctions.matrixFromDouble(iargs.length);
+        double[][] A = iargs[0];
+        if (nargin[0][0] > 0) {
+            A = iargs[0];
+        }
+
+        double[][] res;
+
+
+        if (MCJUtils.isVector(A)) {
+            double[] to_sort = new double[(int)(MCJBaseFunctions.numel(A)[0][0])];
+            for (int i = 1; i <= MCJBaseFunctions.numel(A)[0][0]; i++) {
+                to_sort[i-1]= MCJUtils.kthElemValue(A, i);
+            }
+            res = new double[1][0];
+            Arrays.sort(to_sort);
+            res[0] = to_sort;
+        } else {
+            throw new Exception("not supported yet");
         }
 
       //  MCJUtils.printMatrix(iargs[1]);
