000040c8a7473f49e3d0b508d682b0bb9be528e8
rvlander <gaetan.andre@gmail.com>
2012-06-21 10:21:21 +0200

    traduction of direct_method, resample and init working : YEAH!!!

--- mcbase/reshape.m
+++ mcbase/reshape.m
@@ -1,10 +1,18 @@
 function A=reshape(X,M,N)
 
+if(nargin<3 && numel(M)>1)
+  N = M(2);
+  M = M(1);
+end
+
+
 if(numel(X) ~= M*N) 
     error('reshape : source and target matrix have a different number of elements');
 end
 
 A = zeros(M,N);
+
+
 
 n_elem = M*N;
 
